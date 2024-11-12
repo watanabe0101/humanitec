ca398feafc7da92ea850bce747d29b464574ccd5
渡邊 <info@watanabe-j-p.com>
2024-11-12 23:45:51 +0900

    about追加

---
+++ page-about.php
@@ -0,0 +1,150 @@
+<?php if (!defined('ABSPATH')) exit; ?>
+
+<?php
+/*
+Template Name: about
+*/
+get_header(); ?>
+
+<main>
+  <!-- fv -->
+  <section class="about-header">
+    <div class="about-header__inner">
+      <h1 class="about-header__title">
+        <span class="about-header__sub-title">About US</span>
+        <span class="about-header__main-title">法人概要</span>
+      </h1>
+    </div>
+  </section>
+  <!-- greeting -->
+  <div class="about-greeting">
+    <div class="about-greeting__inner inner">
+      <h2 class="about-greeting__title">ごあいさつ<span class="about-greeting__border"></span></h2>
+      <p class="about-greeting__text">
+        当財団は<span>、</span>大橋学園グループの法人として<span>、</span>おもに介護・保育分野において、社会人向けの教育研修事業のほか<span>、</span>貸研修室の運営や教育コンテンツの作成など<span>、</span>教育関連支援事業を行っております。
+      </p>
+      <p class="about-greeting__text">
+        なかでも<span>、</span>主力の介護福祉士実務者研修においては<span>、</span><span>三重県内各地</span><span>（12か所）</span>をはじめ<span>、</span><span>静岡県東部・伊豆地域</span><span>（5か所）</span><span>、</span>和歌山県新宮市<span>、</span>⾧野県松本市で展開しており<span>、</span>修了者は4,000名にのぼり<span>、</span>人材不足が深刻な介護業界において<span>、</span><span>数多くの介護福祉士の育成に寄与しています。</span><span>（令和6年現在）</span>
+      </p>
+      <p class="about-greeting__text">
+        また<span>、</span>近年では<span>、</span>外国人介護労働者の教育にも注力しており<span>、</span>技能実習管理団体<span>、</span>特定技能支援機関やグループの日本語学校と連携し<span>、</span>外国人向け介護・日本語教育を行ったり<span>、</span>地域の介護事業所様にさまざまな情報提供を行ったりしています。
+      </p>
+      <p class="about-greeting__text">
+        私どもは<span>、</span>介護業界に限らずすべての業界において<span>、</span><span>働く</span><span>「人」</span>が最も大切だと考えており<span>、</span><span>働く人を</span><span>「人材」</span>ではなく<span>、</span><span>「人財」</span>と呼んでいます。
+      </p>
+      <p class="about-greeting__text">
+        <span>教育活動を通じて</span><span>「人財育成」</span>を行うことで、地域の企業や事業所様の発展に貢献できるよう<span>、</span>また、働く人たちが教育を通じてキャリアアップし<span>、</span>誇りをもって仕事に携わることで豊かな人生が送れるよう<span>、</span>教職員一同全力を尽くしてまいりますので<span>、</span>何卒ご支援<span>、</span>ご指導賜りますようお願い申し上げます。
+      </p>
+      <div class="about-greeting__signature">
+        <p class="about-greeting__signature-title">一般財団法人ユマニテク教育支援センター</p>
+        <p class="about-greeting__signature-name"><span class="about-greeting__signature-post">代表理事</span>竹田晃一郎</p>
+      </div>
+    </div>
+  </div>
+  <!-- overview -->
+  <div class="about-overview">
+    <div class="about-overview__inner inner">
+      <!-- table -->
+      <div class="about-overview__details">
+        <table class="about-table">
+          <tbody class="about-table__tbody">
+            <tr class=" about-table__row">
+              <th class="about-table__heading">法人名</th>
+              <td class="about-table__data">一般財団法人ユマニテク教育支援センター</td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading">所在地</th>
+              <td class="about-table__data">三重県四日市市鵜の森一丁目4番28号 ユマニテクプラザ1F</td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading">連絡先</th>
+              <td class="about-table__data">
+                <div class="about-table__phone">
+                  <span class="about-table__tel">電話：059-340-4575</span>
+                  <span class="about-table__fax">FAX：059-351-1711</span>
+                </div>
+              </td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading">代表者</th>
+              <td class="about-table__data">代表理事 竹田 晃一郎</td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading about-table__heading--tall">主な事業</th>
+              <td class="about-table__data about-table__data--tall">
+                <ol class="about-table__list">
+                  <li class="about-table__item--number">社会人向け教育研修事業<br class="br-sp">【ユマニテクキャリアアカデミー】
+                    <ul class="about-table__sub-list">
+                      <li class="about-table__sub-item--circle">介護福祉士実務者研修（厚生労働省指定実務者養成施設）</li>
+                      <li class="about-table__sub-item--circle">保育専門研修</li>
+                      <li class="about-table__sub-item--circle">介護外国人人材集合研修</li>
+                      <li class="about-table__sub-item--circle">外国人（技能実習生／特定技能外国人）向け日本語研修など</li>
+                    </ul>
+                  </li>
+                  <li class="about-table__item--number">教育コンテンツ制作事業</li>
+                  <li class="about-table__item--number">学校法人みえ大橋学園　<br class="br-sp">貸館・貸研修室事業</li>
+                  <li class="about-table__item--number">学校法人みえ大橋学園　<br class="br-sp">ユマニテクプラザ運営管理委託事業</li>
+                  <li class="about-table__item--number">講師派遣業</li>
+                  <li class="about-table__item--number">その他教育活動支援事業</li>
+                </ol>
+              </td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading">取引銀行</th>
+              <td class="about-table__data">三十三銀行　四日市支店</td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading about-table__heading--tall">グループ<br class="br-sp">法人</th>
+              <td class="about-table__data about-table__data--tall">
+                <ul class="about-table__list">
+                  <li class="about-table__item--disc">
+                    <p class="about-table__corporation">学校法人みえ大橋学園</p>
+                    <p class="about-table__group">
+                      ユマニテクライフデザイン専門学校 ／ <br class="br-sp">ユマニテク調理製菓専門学校 ／<br>
+                      ユマニテク看護助産専門学校 ／ <br class="br-sp">専門学校ユマニテク医療福祉大学校 ／ <br class="br-sp">大橋学園高等学校 ／<br>
+                      ユマニテクプラザ
+                    </p>
+                  </li>
+                  <li class="about-table__item--disc">
+                    <p class="about-table__corporation">学校法人大橋学園</p>
+                    <p class="about-table__group">
+                      ユマニテク短期大学 ／ <br class="br-sp">名古屋ユマニテク歯科衛生専門学校 ／ <br class="br-sp">名古屋ユマニテク調理製菓専門学校校 ／<br>
+                      名古屋ユマニテク調理製菓専門学校 高等課程
+                    </p>
+                  </li>
+                  <li class="about-table__item--disc">
+                    <p class="about-table__corporation">社会福祉法人風薫会</p>
+                    <p class="about-table__group">
+                      特別養護老人ホーム サテライトみなと ／ <br class="br-sp">特別養護老人ホーム 風の路 ／<br>
+                      特別養護老人ホーム アリビオ ／ <br class="br-sp">しおはま在宅介護サービスセンター ／<br>
+                      みなと在宅介護サービスセンター
+                    </p>
+                  </li>
+                  <li class="about-table__item--disc">株式会社ユマニテクアソシエイツ</li>
+                </ul>
+              </td>
+            </tr>
+            <tr class="about-table__row">
+              <th class="about-table__heading">設立</th>
+              <td class="about-table__data">平成23年2月22日</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- origin -->
+      <div class="about-origin">
+        <div class="about-origin__image">
+          <img src="<?php echo get_theme_file_uri( '/assets/images/about/about-origin.svg' ); ?>" alt="ユマニテクの由来" loading="lazy">
+        </div>
+        <div class="about-origin__content">
+          <p class="about-origin__text">
+            「ユマニテク」という言葉は、フランス語で人間性をあらわす「ユマニテ（humanité）」と、技や技術の意味をあらわす「テクニーク（Technique）」をあわせて作った造語で、大橋学園グループのすべての短大や専門学校が教育理念として掲げる「豊かな人間性と確かな技術」を通して地域を支える次世代を社会に送り出すということを表しており、当法人もこの名前を引き継いでいます。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<!-- footer -->
+<?php get_footer(); ?>
